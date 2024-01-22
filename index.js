d963a4ae8eeb5fd9b3d173979db3cd4d08479d35
gabeferreiraa <146259686+gabeferreiraa@users.noreply.github.com>
2024-01-22 16:49:32 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,152 @@
+import { animate, stagger, timeline, scroll } from "motion";
+
+// -------- Hamburger Animations --------//
+
+const hamburgerButton = document.getElementById("hamburger-button");
+const hamburgerMenu = document.getElementById("burger-white");
+const hamburgerMenuOpen = document.getElementById("burger-white-2");
+const menuOpenWrapper = document.getElementById("menu-open-wrapper");
+const menuOpenFilter = document.getElementById("menu-open-bg-filter");
+
+hamburgerButton.addEventListener("click", () => {
+  const isOpened = hamburgerButton.getAttribute("aria-expanded");
+
+  if (isOpened === "false") {
+    hamburgerMenuOpen.style.opacity = "100%";
+    menuOpenWrapper.style.display = "flex";
+    hamburgerMenu.style.opacity = "0%";
+    hamburgerButton.setAttribute("aria-expanded", "true");
+  } else {
+    hamburgerMenu.style.opacity = "100%";
+    hamburgerMenuOpen.style.opacity = "0%";
+    hamburgerButton.setAttribute("aria-expanded", "false");
+  }
+});
+
+// -------- Mouse Animations --------//
+
+const cursorDot = document.querySelector("[data-cursor-dot]");
+const cursorOutline = document.querySelector("[data-cursor-outline]");
+
+window.addEventListener("mousemove", function (e) {
+  const posX = e.clientX;
+  const posY = e.clientY;
+
+  cursorDot.style.left = `${posX}px`;
+  cursorDot.style.top = `${posY}px`;
+
+  // cursorOutline.style.left = `${posX}px`;
+  // cursorOutline.style.top = `${posY}px`;
+
+  cursorOutline.animate(
+    {
+      left: `${posX}px`,
+      top: `${posY}px`,
+    },
+    { duration: 200, fill: "forwards" }
+  );
+});
+
+// -------- Set Current Page --------//
+const activePage = window.location.pathname;
+const navLinks = document
+  .querySelectorAll(".heading1-menu-sub-wrapper a")
+  .forEach((link) => {
+    if (link.href.includes(`${activePage}`)) {
+      link.classList.add("w--current");
+    }
+  });
+
+// -------- Animations Start --------//
+const activator = document.querySelector("#hamburger-button");
+// const mover = document.querySelector("#menu-open-wrapper");
+
+// scroll(animate(".progress", { strokeDasharray: ["0,1", "1,1"] }));
+
+let menuOpen = true;
+const daMotion = [
+  [
+    "#menu-open-wrapper",
+    { transform: ["translate3d(0px, 0%, 0px"] },
+    { at: 0.1 },
+  ],
+];
+const daNegativeMotion = [
+  [
+    "#menu-open-wrapper",
+    { transform: ["translate3d(0px, -110%, 0px"] },
+    { at: 0.1 },
+  ],
+];
+
+activator.addEventListener("click", () => {
+  if (menuOpen) {
+    timeline(daMotion, { duration: 0.8 });
+    animate(
+      ".heading1-menu",
+      {
+        y: -40,
+      },
+      { delay: 0.55 },
+      { duration: 0.4 }
+    );
+  } else {
+    timeline(daNegativeMotion, { duration: 0.8 });
+    animate(
+      ".heading1-menu",
+      {
+        y: 40,
+      },
+      {
+        delay: 0.55,
+      },
+      { duration: 0.4 }
+    );
+  }
+
+  menuOpen = !menuOpen;
+});
+
+setTimeout(() => {
+  const loaderText = document.querySelector(".loader").querySelector("h1");
+  const loader = document.querySelector(".loader");
+
+  animate((progress) => (loaderText.innerHTML = Math.round(progress * 100)), {
+    duration: 1,
+    easing: "ease-out",
+  }).finished.then(() => {
+    loader.style.display = "none";
+  });
+}, 700);
+
+const letters =
+  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%&?'";
+
+let interval = null;
+const spanElements = document.querySelectorAll(".hero-text span");
+spanElements.forEach((span) => {
+  span.onmouseover = (event) => {
+    let iteration = 0;
+
+    clearInterval(interval);
+
+    interval = setInterval(() => {
+      event.target.innerText = event.target.innerText
+        .split("")
+        .map((letter, index) => {
+          if (index < iteration) {
+            return event.target.dataset.value[index];
+          }
+
+          return letters[Math.floor(Math.random() * 63)];
+        })
+        .join("");
+
+      if (iteration >= event.target.dataset.value.length) {
+        clearInterval(interval);
+      }
+
+      iteration += 1 / 3;
+    }, 30);
+  };
+});
