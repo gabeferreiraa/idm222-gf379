10dc8d41994600439366fc08d59627d04e7b1c36
gabeferreiraa <146259686+gabeferreiraa@users.noreply.github.com>
2024-03-19 14:45:44 -0400

    added vercel.json2

---
+++ vite.config.js
@@ -0,0 +1,55 @@
+import { defineConfig } from "vite";
+import path from "path";
+
+export default defineConfig({
+  // Base public path when served in development or production.
+  base: "/",
+
+  // Build-specific options
+  build: {
+    // Entry points for multiple page application
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.html"),
+        work: path.resolve(__dirname, "work.html"),
+        contact: path.resolve(__dirname, "contact.html"),
+      },
+      // Additional options here
+    },
+    // Options for outputting files
+    // Example: Separate chunks for vendor libraries
+    outDir: "dist", // Output directory
+    assetsDir: "assets", // Directory for assets within outDir
+    // More options...
+  },
+
+  // Plugins - Add Vite plugins here
+  plugins: [
+    // Example: vue(), reactRefresh()
+  ],
+
+  // Server-specific options for development
+  server: {
+    port: 3000, // Development server port
+    // More options...
+  },
+
+  // Define global variables here
+  define: {
+    // Example: '__APP_VERSION__': JSON.stringify(process.env.npm_package_version),
+  },
+
+  // Resolve options, such as aliasing
+  resolve: {
+    alias: {
+      // Example: {'@': path.resolve(__dirname, 'src')},
+    },
+  },
+
+  // CSS-specific options
+  css: {
+    // Example: Modules options, PostCSS config...
+  },
+
+  // More global configurations...
+});
